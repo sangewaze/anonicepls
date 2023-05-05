ffc3f10f714264c280fcffbd924386a598317549
sangewaze <131881085+sangewaze@users.noreply.github.com>
2023-05-05 11:57:04 +0800

    Update and rename ewlaon to ewlaon.php

---
+++ ewlaon.php
@@ -0,0 +1,27 @@
+$proxy_host = array(
+  "socks5://ams.socks.ipvanish.com:1080",
+  "socks5://atl.socks.ipvanish.com:1080",
+  "socks5://chi.socks.ipvanish.com:1080",
+  "socks5://dal.socks.ipvanish.com:1080",
+  "socks5://den.socks.ipvanish.com:1080",
+  "socks5://fra.socks.ipvanish.com:1080",
+  "socks5://iad.socks.ipvanish.com:1080",
+  "socks5://lax.socks.ipvanish.com:1080",
+  "socks5://lin.socks.ipvanish.com:1080",
+  "socks5://lis.socks.ipvanish.com:1080",
+  "socks5://lon.socks.ipvanish.com:1080",
+  "socks5://mad.socks.ipvanish.com:1080",
+  "socks5://mia.socks.ipvanish.com:1080",
+  "socks5://nrt.socks.ipvanish.com:1080",
+  "socks5://nyc.socks.ipvanish.com:1080",
+  "socks5://par.socks.ipvanish.com:1080",
+  "socks5://phx.socks.ipvanish.com:1080",
+  "socks5://sea.socks.ipvanish.com:1080",
+  "socks5://sin.socks.ipvanish.com:1080",
+  "socks5://sto.socks.ipvanish.com:1080",
+  "socks5://syd.socks.ipvanish.com:1080",
+  "socks5://tor.socks.ipvanish.com:1080",
+  "socks5://waw.socks.ipvanish.com:1080"
+ );
+$proxies = $proxy_host[array_rand($proxy_host)];
+$proxy_userpass = "QcT82nMu8J:RUXWmCYQJ8go";
